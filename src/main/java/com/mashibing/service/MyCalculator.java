fd933552ca2e94c29074342a1ff46a85ae3bac71
xurb <xurongbiao@hmtim.com>
2022-04-27 22:40:16 +0800

    aop例子

--- src/main/java/com/mashibing/service/MyCalculator.java
+++ src/main/java/com/mashibing/service/MyCalculator.java
@@ -2,11 +2,14 @@ package com.mashibing.service;
 
 import com.mashibing.util.LogUtil;
 
+import java.lang.reflect.Method;
+
 public class MyCalculator implements Calculator {
 
     public int add(int i, int j) {
-        LogUtil.start(i,j);
-        int result = i+j;
+        Method add = MyCalculator.class.getMethod("add", int.class, int.class);
+        LogUtil.start(add,i,j);
+        int result = i+j;j
         LogUtil.stop(i,j);
         return result;
     }
--- src/main/java/com/mashibing/util/LogUtil.java
+++ src/main/java/com/mashibing/util/LogUtil.java
@@ -1,10 +1,11 @@
 package com.mashibing.util;
 
+import java.lang.reflect.Method;
 import java.util.Arrays;
 
 public class LogUtil {
-    public static void start(Object ... objects){
-        System.out.println("方法开始执行：参数是"+ Arrays.asList(objects));
+    public static void start(Method method,Object ... objects){
+        System.out.println(method.getName()+"方法开始执行：参数是"+ Arrays.asList(objects));
     }
 
     public static void stop(Object ... objects){
